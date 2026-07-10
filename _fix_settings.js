1d91073c6122b97f63dde06ccf082703459dceb4
ab123c <78955412+Afterglow-404@users.noreply.github.com>
2026-07-11 01:25:24 +0800

    chore: cleanup temp file

--- _fix_settings.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('app/src/main/java/com/aftglw/devapi/SettingsActivity.kt', 'latin1');
-c = c.replace('import com.aftglw.devapi.MoodModel\n', '');
-c = c.replace(
-  'sb.appendLine("Last Mood: ' + '$' + '{com.aftglw.devapi.MoodDetector.lastMood ?: "N/A"}")',
-  'sb.appendLine("Last Mood: ' + '$' + '{com.aftglw.devapi.MoodDetector.lastMood ?: "N/A"} (source: ' + '$' + '{com.aftglw.devapi.MoodDetector.lastSource})")'
-);
-fs.writeFileSync('app/src/main/java/com/aftglw/devapi/SettingsActivity.kt', c, 'latin1');
-console.log('SettingsActivity updated');
